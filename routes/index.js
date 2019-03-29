72bcb776b04d5297e9dc52b069f88e5be4e534ce
Janson <yvrjansonlin@gmail.com>
2019-03-28 18:59:10 -0700

    here are the routes, please look it over! e Please enter the commit message for your changes. Lines starting

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/register', (req, res, next) => {
+  res.render('index');
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('index');
+});
+
+module.exports = router;
