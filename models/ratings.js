4ade7d82675e0ac7a84f977a7960ed67c6d57631
Lucy <lucy13939@gmail.com>
2019-03-28 20:13:00 -0700

    homepage is working
    - deleted node_modules

--- models/ratings.js
+++ models/ratings.js
@@ -1,11 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Ratins = sequelize.define('Ratings', {
+  const Ratings = sequelize.define('Ratings', {
     customer_rating: DataTypes.INTEGER,
     user_rating: DataTypes.INTEGER,
     description: DataTypes.STRING
   }, {});
-  Ratins.associate = function(models) {
+  Ratings.associate = function(models) {
     // associations can be defined here
   };
   return Ratings;
