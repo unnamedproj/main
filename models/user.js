6c032047c867d7cbf6891ae2aa73d84034a0f341
Lucy <lucy13939@gmail.com>
2019-04-13 14:58:50 -0700

    new functions for User db

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 'use strict';
-module.exports = (sequelize, DataTypes) => {
+exports.method = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: DataTypes.STRING,
     password: DataTypes.STRING,
@@ -14,3 +14,42 @@ module.exports = (sequelize, DataTypes) => {
   };
   return User;
 };
+
+module.exports = {
+  findOne: function(user){
+    return new Promise ((resolve, reject) => {
+      const queryString = 'SELECT * FROM Users WHERE username=?';
+      db.query(queryString, [user.username], (err, res) => {
+        if (err) {
+         // send back an error
+          console.log('errorrrrr');
+          reject(err);
+        } else {
+          if (res.length) {
+            console.log('found one');
+            // found a user with username that was passed in
+            resolve(res[0]);
+          } else {
+            console.log('did not find one');
+            // did not find a user with username
+            resolve(false);
+          }
+        }
+      });
+    });
+  },
+
+  create: function(username, email, password, firstName, lastName){
+    return new Promise ((resolve, reject) => {
+      const queryString = 'INSERT INTO Users (name, username, password) VALUES (?, ?, ?)';
+      db.query(queryString, [name, user, pass], (err, res) => {
+        console.log("45:", res, err);
+        if (err) {
+          reject(err);
+        } else {
+          resolve(true);    
+        }
+      });
+    });
+  }
+}
