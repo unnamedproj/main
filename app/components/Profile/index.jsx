cc4b738f86f289b345744b6cf2850cc5064bf24e
Janson <yvrjansonlin@gmail.com>
2019-04-18 19:18:47 -0700

    hardcoded Janson to profile header

--- app/components/Profile/index.jsx
+++ app/components/Profile/index.jsx
@@ -4,12 +4,27 @@ import axios            from 'axios';
 class Profile extends React.Component {
   constructor (props) {
     super (props);
+
+    this.state = {
+      info: {}
+    }
+  }
+
+  componentDidMount() {
+    var x;
+    axios.get('/user/info')
+    .then((info) => {
+      this.setState({
+        info: info.data
+      });
+    });
   }
 
   render() {
+    console.log(this.state);
     return (
       <div>
-      This is the Profile!
+      This is {this.state.info.firstName} Profile!
         //insert html code here
         {/*<Switch>
           <Route path="/login" render={()=><LoginPage onUsernameChange={this.onUsernameChange}/>
