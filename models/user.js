3aa4e50152bb445f7a36eaa4502c1b42097bee59
Lucy <lucy13939@gmail.com>
2019-04-23 20:31:24 -0700

    new columns for db  Please enter the commit message for your changes. Lines starting

--- models/user.js
+++ models/user.js
@@ -2,10 +2,13 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
     password: DataTypes.STRING,
     email: DataTypes.STRING,
     number: DataTypes.STRING,
     availability: DataTypes.STRING,
+    occupation: DataTypes.STRING,
     description: DataTypes.STRING,
     optional_info: DataTypes.STRING
   }, {});
